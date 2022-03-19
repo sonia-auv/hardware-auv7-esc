9e22fd9d20a91d8e069cdd7eac55588c93de9459
Francis Alonzo <44241055+supertoto29@users.noreply.github.com>
2022-03-19 17:55:54 -0400

    add offset of the afro30a / AUV7

--- src/main.cpp
+++ src/main.cpp
@@ -6,39 +6,23 @@
 
 #include "main.h"
 
-RS485 rs(SLAVE_ESC);
-PwmOut pwm[8] = {PwmOut (PWM_1), PwmOut (PWM_2), PwmOut (PWM_3), PwmOut (PWM_4), 
-  PwmOut (PWM_5), PwmOut (PWM_6), PwmOut (PWM_7), PwmOut (PWM_8)};
-Thread threadpwm;
-
 void function_pwm ()
 {
-
   uint8_t cmd_array[1] = {CMD_PWM};
   uint8_t buffer_receiver_pwm[255];
   uint8_t nb_commands = 1;
-  uint8_t size_command = 16;
   
-  uint8_t nb_motor = 8;
-  uint16_t data_pwm_1;
-  uint16_t data_pwm_2;
-  uint8_t i;
-
-  for (uint8_t i =0; i<nb_motor; ++i)
-  {
-    pwm[i].period_us(period);
-    pwm[i].pulsewidth_us(neutralDuty);
-  }
+  uint16_t data_pwm_1, data_pwm_2;
 
   while(1)
   {
-    if (rs.read(cmd_array,nb_commands,buffer_receiver_pwm) == size_command && Killswitch == 0)
+    if (rs.read(cmd_array,nb_commands,buffer_receiver_pwm) == (nb_motor * 2) && Killswitch == 0)
     {
       for(uint8_t i=0; i<nb_motor; ++i)
       {
         data_pwm_1 = buffer_receiver_pwm[(2*i)+1];
         data_pwm_2 = buffer_receiver_pwm[2*i]*256;
-        pwm[i].pulsewidth_us(data_pwm_1+data_pwm_2);
+        pwm[i].pulsewidth_us(data_pwm_1+data_pwm_2 + PWM_OFFSET);
       }
     }
   }
@@ -46,6 +30,12 @@ void function_pwm ()
 
 int main()
 {
+  for (uint8_t i =0; i<nb_motor; ++i)
+  {
+    pwm[i].period_us(period);
+    pwm[i].pulsewidth_us(neutralDuty + PWM_OFFSET);
+  }
+
   threadpwm.start(function_pwm);
   threadpwm.set_priority(osPriorityHigh);
 }
